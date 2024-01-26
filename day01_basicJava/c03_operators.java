a1184468d77e8c7bbefd8f7096d3a62aa6c7d676
Emircan Atçeken <81985622+atcekenemircan@users.noreply.github.com>
2024-01-26 20:07:22 +0300

    Add files via upload

---
+++ day01_basicJava/c03_operators.java
@@ -0,0 +1,18 @@
+package day01_basicJava;
+public class c03_operators {
+    public static void main(String[] args) {
+        /*
+        + ---> aggregation operator
+        - ---> subtraction operator
+        / ---> division operator
+        * ---> multiplication operator
+        % ---> remainder operator
+        */
+        System.out.println(3 + 4.2);
+        System.out.println(3 - 5f);
+        System.out.println(10 / 4.0);
+        System.out.println(3 * 4.5);
+        System.out.println(10 % 4);
+        System.out.println((3f / 4) + (4 * 5));
+    }
+}
